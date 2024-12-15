e53b91e96173577c6b71f02ff8ce91b6acb3ed98
0xk1f0 <dev@k1f0.dev>
2024-12-15 01:23:41 +0100

    fix(day7): optimize runtime and add part 2 solution

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -2,7 +2,6 @@ mod helpers;
 
 use helpers::get_input;
 use itertools::{EitherOrBoth::*, Itertools};
-use std::cmp;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -13,8 +12,8 @@ fn part_one() -> u64 {
     let lines: Vec<String> = BufReader::new(input).lines().map(|l| l.unwrap()).collect();
     // define a vector for statements
     let mut statements: Vec<(u64, Vec<u64>)> = Vec::new();
-    // max length
-    let mut max_length: u64 = 0;
+    // define possible operators
+    let operators = ["+", "*"];
     // collect chars
     lines.iter().for_each(|line| {
         let line: String = line.chars().collect();
@@ -24,62 +23,64 @@ fn part_one() -> u64 {
             .filter(|&particle| particle != "")
             .map(|particle| particle.parse().unwrap())
             .collect();
-        max_length = cmp::max(max_length, numbers.len() as u64);
         statements.push((result_with_numbers[0].parse::<u64>().unwrap(), numbers))
-    });
-    // define operators
-    let mut operators: Vec<&str> = Vec::new();
-    // use max observed length of numbers vector minus two
-    (0..max_length - 2).for_each(|_| {
-        operators.extend(["*", "+"].iter());
     });
     // loop over all found statements
     statements
         .iter()
         .map(|statement| {
+            // keep track of combinations
+            let mut combinations = Vec::new();
+            // determine all possible combinations depending on size
+            for i in 0..operators.len().pow((statement.1.len() - 1) as u32) {
+                let mut sub_combination = Vec::new();
+                let mut index = i;
+                for _ in 0..statement.1.len() - 1 {
+                    let operator = operators[index % operators.len()];
+                    sub_combination.push(operator);
+                    index /= operators.len();
+                }
+                combinations.push(sub_combination);
+            }
             // check for all available combinations
-            // exit as soon aswe have a valid result using any
-            let result = operators
-                .iter()
-                .combinations(statement.1.len() - 1)
-                .unique()
-                .any(|combination| {
-                    let mut result: u64 = 0;
-                    let mut last_operator = "";
-                    // perform calculation
-                    statement
-                        .1
-                        .iter()
-                        .zip_longest(combination)
-                        .for_each(|pair| match pair {
-                            Both(l, &r) => {
-                                match last_operator {
-                                    "+" => {
-                                        result += l;
-                                    }
-                                    "*" => {
-                                        result *= l;
-                                    }
-                                    _ => {
-                                        result += l;
-                                    }
-                                }
-                                last_operator = r;
-                            }
-                            Left(l) => match last_operator {
+            // exit as soon as we have a valid result using any
+            let result = combinations.iter().any(|combination| {
+                let mut result: u64 = 0;
+                let mut last_operator = "";
+                // perform calculation
+                statement
+                    .1
+                    .iter()
+                    .zip_longest(combination)
+                    .for_each(|pair| match pair {
+                        Both(l, &r) => {
+                            match last_operator {
                                 "+" => {
                                     result += l;
                                 }
                                 "*" => {
                                     result *= l;
                                 }
-                                _ => {}
-                            },
-                            Right(_) => {}
-                        });
-                    // check if result equals statement
-                    result == statement.0
-                });
+                                _ => {
+                                    result += l;
+                                }
+                            }
+                            last_operator = r;
+                        }
+                        Left(l) => match last_operator {
+                            "+" => {
+                                result += l;
+                            }
+                            "*" => {
+                                result *= l;
+                            }
+                            _ => {}
+                        },
+                        Right(_) => {}
+                    });
+                // check if result equals statement
+                result == statement.0
+            });
             if result {
                 statement.0
             } else {
@@ -90,7 +91,94 @@ fn part_one() -> u64 {
 }
 
 fn part_two() -> u64 {
-    0
+    // open our input file
+    let input = File::open(get_input()).unwrap();
+    // read out lines
+    let lines: Vec<String> = BufReader::new(input).lines().map(|l| l.unwrap()).collect();
+    // define a vector for statements
+    let mut statements: Vec<(u64, Vec<u64>)> = Vec::new();
+    // define possible operators
+    let operators = ["+", "*", "||"];
+    // collect chars
+    lines.iter().for_each(|line| {
+        let line: String = line.chars().collect();
+        let result_with_numbers: Vec<&str> = line.split(":").collect();
+        let numbers: Vec<u64> = result_with_numbers[1]
+            .split(" ")
+            .filter(|&particle| particle != "")
+            .map(|particle| particle.parse().unwrap())
+            .collect();
+        statements.push((result_with_numbers[0].parse::<u64>().unwrap(), numbers))
+    });
+    // loop over all found statements
+    statements
+        .iter()
+        .map(|statement| {
+            // keep track of combinations
+            let mut combinations = Vec::new();
+            // determine all possible combinations depending on size
+            for i in 0..operators.len().pow((statement.1.len() - 1) as u32) {
+                let mut sub_combination = Vec::new();
+                let mut index = i;
+                for _ in 0..statement.1.len() - 1 {
+                    let operator = operators[index % operators.len()];
+                    sub_combination.push(operator);
+                    index /= operators.len();
+                }
+                combinations.push(sub_combination);
+            }
+            // check for all available combinations
+            // exit as soon as we have a valid result using any
+            let result = combinations.iter().any(|combination| {
+                let mut result: u64 = 0;
+                let mut last_operator = "";
+                // perform calculation
+                statement
+                    .1
+                    .iter()
+                    .zip_longest(combination)
+                    .for_each(|pair| match pair {
+                        Both(l, &r) => {
+                            match last_operator {
+                                "+" => {
+                                    result += l;
+                                }
+                                "*" => {
+                                    result *= l;
+                                }
+                                "||" => {
+                                    result = format!("{}{}", result, l).parse().unwrap();
+                                }
+                                _ => {
+                                    result += l;
+                                }
+                            }
+                            last_operator = r;
+                        }
+                        Left(l) => match last_operator {
+                            "+" => {
+                                result += l;
+                            }
+                            "*" => {
+                                result *= l;
+                            }
+                            "||" => {
+                                result = format!("{}{}", result, l).parse().unwrap();
+                            }
+                            _ => {}
+                        },
+                        Right(_) => {}
+                    });
+                // check if result equals statement
+                result == statement.0
+            });
+            if result {
+                statement.0
+            } else {
+                0
+            }
+        })
+        .sum()
 }
 
 fn main() {
